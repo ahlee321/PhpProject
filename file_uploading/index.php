7ee4b8366a3c0763d3ec5e22d0658ca808d01b6e
ahlee321 <130328584+ahlee321@users.noreply.github.com>
2023-09-23 18:00:21 +0800

    Add files via upload

---
+++ file_uploading/index.php
@@ -0,0 +1,32 @@
+<?php
+include_once 'dbconfig.php';
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Intern msu </title>
+<link rel="stylesheet" href="css/style.css" type="text/css" />
+<meta charset = "UTF-8" />
+		<meta name = "viewport" content = "width=device-width, initial-scale=1" />
+		<link rel = "stylesheet" type = "text/css" href = "css/bootstrap.css" />
+		<link rel = "stylesheet" type = "text/css" href = "css/style.css" />
+		<link rel = "stylesheet" type = "text/css" href = "css/jquery-ui.css" />
+		<link rel = "stylesheet" type = "text/css" href = "css/jquery.dataTables.css" />
+</head>
+<body>
+<?php include '../head.php'; ?>
+   
+    
+<div id="body">
+	<h1>Thanks for applying will notify you 
+    
+    
+    
+    </h1>
+    <a href="../index.php">click | HOME</a>
+</div>
+<div id="footer">
+<label>By <a href="#">penang@tarc.edu.my</a></label>
+</div>
+</body>
+</html>
---
+++ user_registration/header.php
@@ -0,0 +1,11 @@
+<nav class = "navbar navbar-default" style="background-color: lightgrey">
+	<div class = "container-fluid row" style="width: 100%; display: flex;">
+            <img src="https://sureworks.info/storage/files/oTrBSZGApSO50Ta3kdB9p3k9rRIh0VNSG9IE1n1j.png" style="width:350px">		
+            
+            <div href="index.php" style="width: 40%; display: flex; justify-content: end;">
+                    <nav class="navbar bg-body-tertiary">
+                        <h1>TARUMT Internship Portal</h1>
+                    </nav>
+            </div>
+	</div>
+</nav>
