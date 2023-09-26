8dbd11076487514440b245c47958a614653f14c4
ahlee321 <130328584+ahlee321@users.noreply.github.com>
2023-09-26 09:20:22 +0800

    connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,3 @@
 <?php
-	$conn = new mysqli('localhost', 'root', '', 'db_issm') or die(mysqli_error());
+	$conn = new mysqli('database1.chzwyhfvurav.us-east-1.rds.amazonaws.com', 'admin', 'password', 'database1') or die($conn->error);
 ?>
