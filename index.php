e7efcc53162ecbc693ee470165d64fca98ba8eb7
ahlee321 <130328584+ahlee321@users.noreply.github.com>
2023-09-26 09:21:20 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <?php
-	require 'connect.php';
+	$conn = new mysqli('database1.chzwyhfvurav.us-east-1.rds.amazonaws.com', 'admin', 'password', 'database1') or die($conn->error);
 ?>
 <html lang = "en">
 	<head>
