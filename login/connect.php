5b5eef9708480d7344d682b3ff964629a3601383
ahlee321 <130328584+ahlee321@users.noreply.github.com>
2023-09-26 09:25:23 +0800

    Update connect.php

--- login/connect.php
+++ login/connect.php
@@ -1,5 +1,5 @@
 <?php
-$connect=mysqli_connect('localhost','root','','db_issm');
+$connect=new mysqli('database1.chzwyhfvurav.us-east-1.rds.amazonaws.com', 'admin', 'password', 'database1') or die($conn->error);
  
 if(mysqli_connect_errno($connect))
 {
