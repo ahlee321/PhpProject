ff38b617e72bb3eb38dd9995654af0e7a006bce9
ahlee321 <130328584+ahlee321@users.noreply.github.com>
2023-09-26 21:12:31 +0800

    Update dbconfig.php

--- file_uploading/dbconfig.php
+++ file_uploading/dbconfig.php
@@ -1,8 +1,8 @@
 <?php
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "db_issm";
+$dbhost = "database1.chzwyhfvurav.us-east-1.rds.amazonaws.com";
+$dbuser = "admin";
+$dbpass = "password";
+$dbname = "database1";
 $conn= mysqli_connect($dbhost,$dbuser,$dbpass) or die('cannot connect to the server'); 
 mysqli_select_db($conn,$dbname) or die('database selection problem');
 ?>
