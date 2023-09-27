8657bff9ea463fc5f1b3ca297963e67eb66013e2
ahlee321 <130328584+ahlee321@users.noreply.github.com>
2023-09-28 00:13:01 +0800

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -13,4 +13,59 @@
 		<li><h4>Manifest interpersonal, communications, and leadership skills through significant curricular, co-curricular and extra-curricular activities</h4></li>
 		<li><h4>Virtues social consciousness through active participation in research, extension and professional organization</h4></li>
 	</ol>
+	<div class = "alert alert-success"><center><h3>Portfolio</h3></center></div>
+
+        <div style="display: flex;">
+            <section class="team-member">
+                <div class="profileImage">
+                    <div class="circle">
+                        <img src="https://i.imgur.com/cmmB4bV.jpg" alt="Team Member 1">
+                    </div>
+                </div>
+            
+                <h2>Ong Hao Zhe</h2>
+                <p>Cloud Solutions Architect</p >
+                <br/>
+                <p>Ong Hao Zhe is our Cloud Solutions Architect with extensive experience in designing and implementing cloud-based solutions. He specializes in crafting scalable and secure cloud architectures that empower businesses to thrive in the digital age.</p >
+            </section>
+            
+            <section class="team-member">
+                <div class="profileImage">
+                    <div class="circle">
+                        <img src="https://i.imgur.com/RBEk0mM.png" alt="Team Member 2">
+                    </div>
+                </div>
+            
+                <h2>Lee Chun Fong</h2>
+                <p>Cloud Infrastructure Designer</p >
+                <br/>
+                <p>Lee Chun Fong is our Cloud Infrastructure Designer who excels in creating robust and efficient cloud infrastructure. With a keen eye for optimizing resources, he ensures our clients' cloud environments are finely tuned for performance and cost-effectiveness.</p >
+            </section>
+            
+            <section class="team-member">
+                <div class="profileImage">
+                    <div class="circle">
+                        <img src="https://i.imgur.com/MhXC5vm.jpeg" alt="Team Member 3">
+                    </div>
+                </div>
+            
+                <h2>Lim Well Shen</h2>
+                <p>Cloud Marketing Specialist</p >
+                <br/>
+                <p>Lim Well Shen is our Cloud Marketing Specialist, dedicated to spreading awareness about the benefits of cloud technology. He devises innovative marketing strategies to connect businesses with the right cloud solutions to accelerate their growth.</p >
+            </section>
+            
+            <section class="team-member">
+                <div class="profileImage">
+                    <div class="circle">
+                        <img src="https://i.imgur.com/O6sYB5e.png" alt="Team Member 4">
+                    </div>
+                </div>
+            
+                <h2>Eng Kean Chew</h2>
+                <p>Cloud Content Strategist</p >
+                <br/>
+                <p>Eng Kean Chew is our Cloud Content Strategist, responsible for creating engaging and informative content that educates our audience on cloud technologies. His content empowers businesses to make informed decisions in their cloud journey.</p >
+            </section>
+        </div>
 </div>
