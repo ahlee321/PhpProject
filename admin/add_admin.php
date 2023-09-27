a53106a68bbc729ee96356ef9cb71bff28467d7f
ahlee321 <130328584+ahlee321@users.noreply.github.com>
2023-09-27 23:45:39 +0800

    Update add_admin.php

--- admin/add_admin.php
+++ admin/add_admin.php
@@ -5,12 +5,13 @@
 		$password = $_POST['password'];
 		$name = $_POST['name'];
 		$a_query = $conn->query("SELECT * FROM `admin` WHERE `username` = '$username'") or die($conn->error);
+		
 		$a_valid = $a_query->num_rows;
 		if($a_valid > 0){
 			echo "<script>alert('Username already taken')</script>";
 			echo "<script>window.location = 'admin.php'</script>";
 		}else{
-			$conn->query("INSERT INTO `admin` VALUES('', '$username', '$password', '$name')") or die($conn->error);
+			$conn->query("INSERT INTO `admin` VALUES(NULL,'$username', '$password', '$name')") or die($conn->error);
 			header('location:admin.php');
 		}
 	}	
