283ceb6cf06fe6f48889f9c4774569e6c2e53318
ahlee321 <130328584+ahlee321@users.noreply.github.com>
2023-09-27 10:50:39 +0800

    Update add_activity.php

--- admin/add_activity.php
+++ admin/add_activity.php
@@ -8,7 +8,7 @@
 		$month = date("M", strtotime($_POST['start']));
 		$year = date("Y", strtotime($_POST['start']));
                 $compname = $_POST['compname'];
-                $conn->query("INSERT INTO activity VALUES('', '$title', '$description', '$start', '$end', '$month', '$year','$compname')") or die(mysqli_error());
+                $conn->query("INSERT INTO activity VALUES(NULL, '$title', '$description', '$start', '$end', '$month', '$year','$compname')") or die(mysqli_error());
                 header('location: activity.php');
 	}
 ?>
